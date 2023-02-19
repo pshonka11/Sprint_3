72a14ad949cf604f42aa12319157e781fc46696d
Alena Vezdeneva <pshonka11@yandex.ru>
2023-02-19 21:44:52 +0300

    Sprint_3

--- Tests1/log_out_via_exit_in_personal_room.py
+++ Tests1/log_out_via_exit_in_personal_room.py
@@ -6,8 +6,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 driver = webdriver.Chrome()
 driver.get("https://stellarburgers.nomoreparties.site")
 
-# Найти кнопку "Войти" и нажать её
-driver.find_element(By.XPATH, '//*[@id="root"]/div/main/section[2]/div/button').click()
+# Найти кнопку "Личный кабинет" и нажать её
+driver.find_element(By.XPATH, '//*[@id="root"]/div/header/nav/a/p').click()
 
 # Ввести логин и пароль
 driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[1]/div/div/input').send_keys('alena_vezdeneva_6_111@yandex.ru')
@@ -15,12 +15,6 @@ driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[2]/div
 
 # Нажать кнопку "Войти"
 driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button').click()
-
-# Нажать кнопку "Личный кабинет"
-driver.find_element(By.XPATH, '//*[@id="root"]/div/header/nav/a').click()
-
-# Нажать кнопку "Выход"
-driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/nav/ul/li[3]/button').click()
 
 # Закрыть браузер
 driver.quit()
