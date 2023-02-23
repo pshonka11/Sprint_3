039a267b4bd35d80dc19777f13527371f2352134
Alena Vezdeneva <pshonka11@yandex.ru>
2023-02-23 23:46:34 +0300

    Sprint_3

--- Tests1/Registration.py
+++ Tests1/Registration.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_registration():
     driver = webdriver.Chrome()
@@ -15,15 +16,15 @@ def test_registration():
 
 # Заполнить поля с именем, почтой и паролем
     driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[1]/div/div/input').send_keys('Alena')
-    driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input').send_keys('alena_vezdeneva_6_111@yandex.ru')
-    driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[3]/div/div/input').send_keys('123456')
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input').send_keys('alena_vezdeneva_6_114@yandex.ru')
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[3]/div/div/input').send_keys('123454')
 
 # Найти и нажать кнопку "Зарегистрироваться"
     driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/form/button').click()
 
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, ".//button[text()='Войти']")))
 
-#проверяем что регистрация прошла успешно - переход на страницу авторизации
+# проверяем что регистрация прошла успешно - переход на страницу авторизации
     current_url = driver.current_url
     assert 'login' in current_url
 
